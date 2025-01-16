3eb12c4957afbca678bcd83d114e97351bb7978f
kevinseya <kevinseya@gmail.com>
2025-01-16 00:06:19 -0500

    fix: :bug: Update config with swagger title

--- src/main/java/com/microserviciologistic/updateuser/config/SwaggerConfig.java
+++ src/main/java/com/microserviciologistic/updateuser/config/SwaggerConfig.java
@@ -12,8 +12,8 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("User create API")
+                        .title("User update API")
                         .version("1.0.0")
-                        .description("API for create users"));
+                        .description("API for update users"));
     }
 }
